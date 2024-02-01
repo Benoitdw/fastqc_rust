d821baf3e792b3e0561d32d31ac1c1691449998a
Benoitdw <bw@oncodna.com>
2024-02-01 19:47:42 +0100

    Read fastq read

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use fastq::{parse_path, Record};
+
+#[derive(Parser)]
+struct Cli {
+    fastq_path : String,
+}
+
+fn main() {   
+    let args = Cli::parse();  
+    
+    let result = parse_path(Some(args.fastq_path), |parser| {
+        parser.each(|record| {
+            let record_as_str : String = record.seq().iter().map(|&byte| char::from(byte)).collect();
+            println!("{:?}",record_as_str);
+            true
+        })
+    });
+    match result{
+        Ok(_result) => println!("Fastq correctly handled."),
+        Err(error) => println!("There was an error handling fastq. {:?}", error)
+    };
 }
