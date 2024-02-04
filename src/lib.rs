f3ed8105a3961e6a187160a38bd91634c09535cd
Benoitdw <bw@oncodna.com>
2024-02-04 15:58:41 +0100

    quality wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,38 @@
+use std::{result, vec};
+
 use fastq::{parse_path, Record};
 
+struct Resume {
+    raw_quality_count: Vec<Vec<u8>>,
+    base_average_quality: Vec<f32>,
+}
+
+impl Resume {
+    fn new() -> Self {
+        Self {
+            raw_quality_count: Vec::new(),
+            base_average_quality: Vec::new(),
+        }
+    }
+
+    fn compute_stats(&mut self) {
+        self.raw_quality_count
+            .iter()
+            .for_each(|base| self.base_average_quality.push(average(base)))
+    }
+}
+
 pub fn parse_fastq(fastq_path: String) {
+    let mut resume = Resume::new();
     let result = parse_path(Some(fastq_path), |parser| {
         parser.each(|record| {
-            let record_as_str: String = record.seq().iter().map(|&byte| char::from(byte)).collect();
-            println!("{:?}", record_as_str);
+            record.qual().iter().enumerate().for_each(|(id, &qual)| {
+                match resume.raw_quality_count.get_mut(id) {
+                    Some(vector) => vector.push(qual),
+                    None => resume.raw_quality_count.push(vec![qual]),
+                }
+                resume.raw_quality_count[id].push(qual)
+            });
             true
         })
     });
@@ -12,4 +40,19 @@ pub fn parse_fastq(fastq_path: String) {
         Ok(_result) => println!("Fastq correctly handled."),
         Err(error) => println!("There was an error handling fastq. {:?}", error),
     };
+    resume.compute_stats();
+    println!(
+        "Average quality:  {:?}",
+        resume
+            .base_average_quality
+            .iter()
+            .map(|v| { *v as u32 })
+            .sum::<u32>() as f32
+            / resume.base_average_quality.len() as f32
+    )
+}
+
+// I want this function to be able to take any type of int in vector
+fn average(vector: &Vec<u8>) -> f32 {
+    vector.iter().map(|v| *v as u32).sum::<u32>() as f32 / vector.len() as f32
 }
